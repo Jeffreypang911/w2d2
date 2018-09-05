2e256ae4d3502c2c11a0d1c3d92c95d06febec0f
jeffreypang911 <jeffreypang911@gmail.com>
2018-09-05 23:31:50 +0000

    cleaned up code formatting

--- express_server.js
+++ express_server.js
@@ -13,28 +13,15 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-// req = request sent by client
-// res = response sent by us (the server)
-
-// each of these "app.get" functions are called root handlers! :D
-
+// handlers/endpoints
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
   })
-//the above knows to checks inside "views" folder for "urls_index", automatically looks for ejs file too!
-
-///Important: When sending variables to an EJS template, you need to send them inside
-//an object, even if you are only sending one variable. This is so you can use the key
-//of that variable (in the above case the key is urls) to access the data within your
-//template.
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-//the above renders out the urls_new ejs file with the forum
-
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shorturl: req.params.id, longurl: urlDatabase[req.params.id] };
@@ -45,41 +32,31 @@ app.post("/urls/:id", (req, res) => {
   let templateVars = { shorturl: req.params.id, longurl: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
-
 //above :id value in the quotations allows us to input the id in the
 //browser and pull up the corrisponding website to that shortened url.
 //NOTE: shorturl and urls are variables that can be referenced in the urls_shows ejs file.
 app.post("/urls", (req, res) => {
   RandomNumber = generateRandomString()
   urlDatabase[RandomNumber] = req.body.longURL
-  console.log(urlDatabase)
-  res.redirect(`http://localhost:8080/urls/${RandomNumber}`);         // Respond with 'Ok' (we will replace this)
+  res.redirect(`http://localhost:8080/urls/${RandomNumber}`);
 });
 //above uses values longURL from the urls_new EJS file.
 //calls RandomNumber function and then creates "Random Number" key inside
-//Database. Then assigns req.body.longURL to that key value.
+//Daatbase. Then assigns req.body.longURL to that key value.
 app.post("/urls/:id/edit", (req, res) => {
   urlDatabase[req.params.id] = req.body.editURL
-  console.log(urlDatabase)
-
   res.redirect(`http://localhost:8080/urls/`)
 });
-
-
-
+//redirects website
 app.get("/u/:shortURL", (req, res) => {
   var tinyURL = req.params.shortURL
   var longURL = urlDatabase[tinyURL]
   res.redirect(`http://${longURL}`);
 });
-//above redirects to website
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 })
-
-// app.post("/urls/edit", (req, res) => {
-// }) EDIT POST
 
 app.post("/urls/:id/delete", (req, res) => {
   let tinyURL = req.params.id
